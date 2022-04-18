b2158994f948b7171ba867851f3d2bea7e636ead
Jonnywdev <jonnywhitt19@gmail.com>
2022-04-18 20:04:01 +0000

    Add initial welcome message

--- run.py
+++ run.py
@@ -1,3 +1,13 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def main():
+    """
+    This is the main function that will be used to run the game and
+    it will have other function such as a way to get the username!
+    """
+
+    print("Welcome to Rock | paper | scissors!")
+    print("rules: \n")
+    print("rock beats scissors, paper beats \
+    rock and scissors beats paper.")
+    print("Play by choosing and entering either rock, paper or scissors.")
+    print("Please make sure that you enter one of the 3 \
+        options as any other entry will not be valid.\n")
